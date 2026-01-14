cc1349b54f03338a01067c0f53156f9a07f8161c
Er3c <18633847058@163.com>
2026-01-14 14:01:24 +0800

    fix: update site URL to correct Cloudflare Pages domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://project-51.pages.dev',
+  site: 'https://project-51-39r.pages.dev',
   output: 'server',
   adapter: cloudflare({
     platformProxy: {
